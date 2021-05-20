d4a3768bbf260ab32c6c010a3236f80244009c5d
Rayron Rodffer <r.rodffer@gmail.com>
2021-05-20 00:19:43 -0300

    feat: desafio 2 concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const userExists = users.find(user => user.username === username)
+
+  if (!userExists) 
+    return response.status(404).json({ error: 'Usuario nao encontrado' })
+
+  request.user = userExists
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  const userHasQuota = function () {
+    return (!user.pro && user.todos.length < 10) || user.pro
+  }
+
+  if (!userHasQuota()) 
+    return response.status(403).json({ error: 'Limite de 10 todo atingido' })
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+  if (!user)
+    return response.status(404).json({ error: 'Usuario nao encontrado' })
+
+  const isValidId = validate(id)
+  if (!isValidId)
+    return response.status(400).json({ error: 'Id invalido' })
+
+  const todo = user.todos.find(todo => todo.id === id)
+  if (!todo)
+    return response.status(404).json({ error: 'Id nao encotrado' })
+
+  request.user = user
+  request.todo = todo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const userExists = users.find(user => user.id === id)
+
+  if (!userExists) 
+    return response.status(404).json({ error: 'Usuario nao encontrado' })
+
+  request.user = userExists
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
